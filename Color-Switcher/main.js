0b73baba92453aecc5aa9129632dfcd801d52f1c
Haris Altaf <62622950+harix76@users.noreply.github.com>
2025-08-10 22:49:48 +0000

    Solution One Implemented

---
+++ Color-Switcher/main.js
@@ -0,0 +1,25 @@
+const buttons = document.querySelectorAll('.button');
+const body = document.querySelector('body');
+
+buttons.forEach(function (eachButton) {
+  eachButton.addEventListener('click', function (event) {
+    console.log(event);
+    console.log(event.target);
+    switch (event.target.id) {
+      case 'grey':
+        body.style.backgroundColor = event.target.id;
+        break;
+      case 'white':
+        body.style.backgroundColor = event.target.id;
+        break;
+      case 'blue':
+        body.style.backgroundColor = event.target.id;
+        break;
+      case 'yellow':
+        body.style.backgroundColor = event.target.id;
+        break;
+      default:
+        break;
+    }
+  });
+});
